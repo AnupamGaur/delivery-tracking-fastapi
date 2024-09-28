cd1199eb06ca44ff392c6cae287d0d4858fbc418
AnupamGaur <86046454+AnupamGaur@users.noreply.github.com>
2024-09-28 16:34:37 +0530

    Initial commit

--- main.py
+++ main.py
@@ -10,23 +10,3 @@ app = FastAPI()
 app.include_router(page_router)
 
 
-@lru_cache
-def get_settings():
-    return Settings()
-
-@app.get("/info")
-async def info(settings: Annotated[Settings, Depends(get_settings)]):
-    return {
-        "app_name": settings.FEDEX_SECRET_KEY,
-        "admin_email": settings.FEDEX_API_KEY,
-        "items_per_user": settings.FEDEX_BASE_API_URL,
-    }
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-async def read_item(item_id:int):
-    return {"item_id": item_id}
